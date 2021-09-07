ccfd43df822c39ba856e3191dfacc1a75bb13069
nando <fmfernando61@gmail.com>
2021-09-06 23:02:07 -0300

    update component home

---
+++ src/Components/Disclosure/Disclosure.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Disclosure.css";
+
+function Disclosure() {
+  return (
+    <>
+      <div className='disclosure-container'>
+        <div className='disclosure-wrapper'>
+          <div className='disclosure-text'>
+            <h1>TITULO</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Disclosure;
