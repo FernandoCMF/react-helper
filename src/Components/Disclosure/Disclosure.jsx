b47f93da7977fddfd13915fce1c4734513bbc61f
nando <fmfernando61@gmail.com>
2021-09-09 20:47:30 -0300

    implementation component disclosure

--- src/Components/Disclosure/Disclosure.jsx
+++ src/Components/Disclosure/Disclosure.jsx
@@ -3,15 +3,17 @@ import "./Disclosure.css";
 
 function Disclosure() {
   return (
-    <>
-      <div className='disclosure-container'>
-        <div className='disclosure-wrapper'>
-          <div className='disclosure-text'>
-            <h1>TITULO</h1>
-          </div>
-        </div>
+    <div className='disclosure__container'>
+      <div className='disclosure__info-first'>
+        <h1>TEXTO 1</h1>
       </div>
-    </>
+      <div className='disclosure__info-second'>
+        <h1>TEXTO 2</h1>
+      </div>
+      <div className='disclosure__info-third'>
+        <h1>TEXTO 3</h1>
+      </div>
+    </div>
   );
 }
 
