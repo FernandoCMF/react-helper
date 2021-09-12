74f7ae5e629a8113444aee94db2b7d23006474f9
nando <fmfernando61@gmail.com>
2021-09-12 19:33:34 -0300

    Adjustments to the layout

--- src/Components/Disclosure/Disclosure.jsx
+++ src/Components/Disclosure/Disclosure.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import ImageLogo from "../../Assets/Imagens/logo.svg";
+import Image2 from "../../Assets/Imagens/img-2.jpg";
 import "./Disclosure.css";
 
 function Disclosure() {
   return (
     <div className='disclosure__container'>
-      <div className='disclosure__info-first'>
-        <h1>TEXTO 1</h1>
-      </div>
-      <div className='disclosure__info-second'>
-        <h1>TEXTO 2</h1>
-      </div>
-      <div className='disclosure__info-third'>
-        <h1>TEXTO 3</h1>
+      <div className='disclosure__wrapper'>
+        <div className='disclosure__wrapper-info'>
+          <h1>
+            Juntos somos mais f<img className='logo' src={ImageLogo} />
+            rtes!
+          </h1>
+          <div className='info__wrapper'>
+            <div className='info__wrapper-text'>
+              <h3>A VIDA NÃO PARA</h3>
+              <p>
+                A luta contra a pobreza não pode parar. Doando apenas R$ 2.00
+                por dia você pode mudar a vida de uma criança e de toda sua
+                comunidade. Sua doação vai facilitar, por exemplo, que famílias
+                tenham acesso a direitos básicos, como à água e à alimentação
+                saudável.
+              </p>
+              <h3>FAÇA UMA CRIANÇA FELIZ</h3>
+            </div>
+            <img
+              src={Image2}
+              alt='Children smile'
+              className='info__wrapper-image'
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
