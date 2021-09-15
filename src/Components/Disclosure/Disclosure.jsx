c4e2c5d89a47d8ea629af854e3aaf49633729364
nando <fmfernando61@gmail.com>
2021-09-15 15:05:25 -0300

    Conclued disclosure component

--- src/Components/Disclosure/Disclosure.jsx
+++ src/Components/Disclosure/Disclosure.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ImageLogo from "../../Assets/Imagens/logo.svg";
 import Image2 from "../../Assets/Imagens/img-2.jpg";
+
 import "./Disclosure.css";
 
 function Disclosure() {
@@ -9,12 +10,14 @@ function Disclosure() {
       <div className='disclosure__wrapper'>
         <div className='disclosure__wrapper-info'>
           <h1>
-            Juntos somos mais f<img className='logo' src={ImageLogo} />
+            Juntos somos mais f
+            <img className='logo' alt='children' src={ImageLogo} />
             rtes!
           </h1>
           <div className='info__wrapper'>
             <div className='info__wrapper-text'>
               <h3>A VIDA NÃO PARA</h3>
+
               <p>
                 A luta contra a pobreza não pode parar. Doando apenas R$ 2.00
                 por dia você pode mudar a vida de uma criança e de toda sua
@@ -22,7 +25,8 @@ function Disclosure() {
                 tenham acesso a direitos básicos, como à água e à alimentação
                 saudável.
               </p>
-              <h3>FAÇA UMA CRIANÇA FELIZ</h3>
+
+              <h3>FAÇA UMA CRIANÇA FELIZ!</h3>
             </div>
             <img
               src={Image2}
